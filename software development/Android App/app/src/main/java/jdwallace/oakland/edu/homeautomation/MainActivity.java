f4a6967d9c8e332f7735a218d23523dd3f593b64
Jeff Wallace <jwallace121988@gmail.com>
2018-01-07 14:14:01 -0500

    Test calculator java app... check it out Jheryl

--- software development/Android App/app/src/main/java/jdwallace/oakland/edu/homeautomation/MainActivity.java
+++ software development/Android App/app/src/main/java/jdwallace/oakland/edu/homeautomation/MainActivity.java
@@ -1,13 +1,41 @@
 package jdwallace.oakland.edu.homeautomation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public TextView resultTextView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button AddButton = (Button) findViewById(R.id.AddButton);
+        AddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText firstNumEditText = (EditText) findViewById(R.id.FirstNumEditText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.SecondNumEditText);
+                resultTextView = (TextView) findViewById(R.id.ResultTextView);
+                int num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumEditText.getText().toString());
+                int result = num1 + num2;
+                resultTextView.setText(result + "");
+            }
+        });
+
+        Button ClearButton = (Button) findViewById(R.id.ClearButton);
+        ClearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resultTextView = (TextView) findViewById(R.id.ResultTextView);
+                resultTextView.setText("RESULT");
+            }
+        });
     }
 }
